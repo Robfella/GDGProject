56cc08445bccc182c7a3a1592061ae7d921bd1cf
Robert Martin <rmartin3@unca.edu>
2015-10-23 15:32:33 -0400

    Made some changes!
    Made a floating script
    Added some textures
    Changed the tessellation.

---
+++ Source/GDGProject/FloatingActor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GDGProject.h"
+#include "FloatingActor.h"
+
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	FVector NewLocation = GetActorLocation();
+	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+	NewLocation.Z += DeltaHeight * 20.0f;      
+	NewLocation.X += DeltaHeight * 1.6f;
+	NewLocation.Y += DeltaHeight * .6f;
+	RunningTime += DeltaTime;
+	SetActorLocation(NewLocation);
+}
+
